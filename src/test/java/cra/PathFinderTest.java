f8788d19609e8162505867d75b74cb7311cc759b
Andreas Voß <Andreas_Voss@tonbeller.com>
2016-06-01 15:04:04 +0200

    added more tests

--- src/test/java/cra/PathFinderTest.java
+++ src/test/java/cra/PathFinderTest.java
@@ -15,32 +15,39 @@ import static org.assertj.core.api.Assertions.*;
 
 @RunWith(Parameterized.class)
 public class PathFinderTest {
-  
-  
+
+
   private String name;
 
   public PathFinderTest(String name) {
     this.name = name;
   }
-  
+
   @Parameters(name = "Test = {0}")
   public static Iterable<Object[]> data() {
-    return Arrays.asList(new Object[][] { 
-             { "impossible" },
-             { "simplest" },
-             { "twocorners" },
-             { "alldirections" },
+    return Arrays.asList(new Object[][] {
+             { "simple_2_2" },
+             { "twocorners_3_3" },
+             { "impossible_3_3" },
+             { "oneBestPath_11_17" },
+             { "small_1_1" },
+             { "multipleGoodPaths_10_5" },
+             { "impossible_20_10" },
+             { "manyGoodPaths_10_10" },
+             { "multipleGoodPaths_40_20" },
+             { "empty_20_15" },
+             { "alldirections_6_7" },
      });
   }
-  
+
   @Test
   public void assertThatActualSolutionIsContainedInValidSolutions() throws IOException {
     PathFinder finder = new PathFinder();
     String maze = loadResource(name + ".maze");
-    String actualSolution = finder.findOptimalPath(maze);  
+    String actualSolution = finder.findOptimalPath(maze);
     String validSolutions = loadResource(name + ".solutions");
-    assertThat(validSolutions).contains(actualSolution);
-    
+    assertThat(validSolutions).describedAs("valid solutions should contain your solution").contains(actualSolution);
+
   }
 
   private String loadResource(String name) throws IOException {
